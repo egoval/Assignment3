92ca62357b46c69cc49fc9080537cae7631552ef
egoval <brandon.gangerdeen@gmail.com>
2024-11-24 19:55:28 -0500

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -1,11 +1,13 @@
 const express = require('express'); // Import Express framework
 const bodyParser = require('body-parser'); // Middleware to parse form data
+const methodOverride = require('method-override');// Allow PUT and DELETE in forms
 const path = require('path');// Utility to handle file paths
 
 const app = express(); // Create an instance of the Express application
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, '../../public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
